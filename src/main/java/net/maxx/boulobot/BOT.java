77db1f366763a874f4809b04207296d32cf914eb
Maxx <djmax@lotary.net>
2019-11-15 22:20:42 +0100

    Started to edit the notification (currently, the code isn't working as the command &notif on twitch is executing the method without passing the correct argument, and I have to figure out how can I make it work properly.)

--- src/main/java/net/maxx/boulobot/BOT.java
+++ src/main/java/net/maxx/boulobot/BOT.java
@@ -9,6 +9,7 @@ import com.github.twitch4j.auth.providers.TwitchIdentityProvider;
 import com.github.twitch4j.chat.events.channel.IRCMessageEvent;
 import com.github.twitch4j.common.events.channel.ChannelGoLiveEvent;
 import com.github.twitch4j.common.events.channel.ChannelGoOfflineEvent;
+import com.github.twitch4j.helix.domain.GameList;
 import net.dv8tion.jda.api.*;
 import net.dv8tion.jda.api.entities.*;
 import net.maxx.boulobot.commands.CommandMap;
@@ -20,6 +21,7 @@ import net.maxx.boulobot.util.*;
 import javax.security.auth.login.LoginException;
 import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -146,7 +148,9 @@ public class BOT implements Runnable{
     private void loadNotifications() {
         twitchClient.getClientHelper().enableStreamEventListener(channelName);
         twitchClient.getEventManager().onEvent(ChannelGoLiveEvent.class).subscribe(this::sendGoLiveNotif);
-        twitchClient.getEventManager().onEvent(ChannelGoOfflineEvent.class).subscribe(this::sendGoOfflineNotif);
+        twitchClient.getEventManager().onEvent(ChannelGoOfflineEvent.class).subscribe(channelGoOfflineEvent -> {
+            sendGoOfflineNotif();
+        });
     }
 
     public void sendGoLiveNotif(ChannelGoLiveEvent channelGoLiveEvent){
@@ -158,15 +162,23 @@ public class BOT implements Runnable{
         embedBuilder.setFooter(Reference.EmbedFooter.asDate(), Reference.EmbedIcon.getString());
         embedBuilder.setColor(3066993);
         embedBuilder.setDescription("Oyé oyé les "+notif.getAsMention()+" !\n**"+lyorine.getAsMention()+"** part en stream !\n» https://twitch.lyorine.com");
+        embedBuilder.addField(new MessageEmbed.Field("Titre", channelGoLiveEvent.getTitle(), true));
+        GameList resultList = twitchClient.getHelix().getGames(Collections.singletonList(channelGoLiveEvent.getGameId()), null).execute();
+        final String[] gameName = {"Aucun jeu"};
+        resultList.getGames().forEach(game -> {
+            gameName[0] = game.getName();
+            embedBuilder.setImage(game.getBoxArtUrl());
+        });
+        embedBuilder.addField(new MessageEmbed.Field("Jeu", gameName[0], true));
         embedBuilder.setThumbnail(Reference.BellImage.getString()+"?size=256");
         TextChannel toSend = jda.getGuildById(Reference.GuildID.getString()).getTextChannelById(Reference.NotifTextChannelID.getString());
         Message message = new MessageBuilder(notif.getAsMention()).setEmbed(embedBuilder.build()).build();
         toSend.sendMessage(message).queue();
         twitchClient.getChat().sendMessage(channelName, "Coucou imGlitch ! \n» Je viens d'envoyer la notification à tous les chats ! Bon live ! LUL");
-        jda.getPresence().setActivity(Activity.streaming("avec sa reine", "https://twitch.tv/"+channelName.toUpperCase()));
+        jda.getPresence().setActivity(Activity.streaming("avec sa reine à "+gameName, "https://twitch.tv/"+channelName.toUpperCase()));
     }
 
-    public void sendGoOfflineNotif(ChannelGoOfflineEvent channelGoOfflineEvent){
+    public void sendGoOfflineNotif(){
         logger.log(Level.INFO, "> Le stream est OFFLINE!");
         EmbedBuilder embedBuilder = new EmbedBuilder();
         embedBuilder.setTitle("Notification \uD83D\uDD14", "https://twitch.tv/"+channelName.toUpperCase());

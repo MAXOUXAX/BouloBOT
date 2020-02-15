964a181b8d7be62b1137e58f609e290e0205501a
MAXOUXAX <djmax486@gmail.com>
2020-02-15 19:19:21 +0100

    Added some data on the session object

--- src/main/java/net/maxx/boulobot/util/Session.java
+++ src/main/java/net/maxx/boulobot/util/Session.java
@@ -1,11 +1,23 @@
 package net.maxx.boulobot.util;
 
+import com.github.twitch4j.helix.domain.GameList;
+import com.github.twitch4j.helix.domain.Stream;
+import com.github.twitch4j.helix.domain.StreamList;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.*;
+import net.maxx.boulobot.BOT;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.UUID;
+import java.util.logging.Level;
 
 public class Session {
 
+    private BOT botDiscord;
     private UUID uuid;
+    private String channelId;
     private long startDateMillis;
     private long endDateMillis;
     private int maxViewers;
@@ -13,11 +25,20 @@ public class Session {
     private int commandUsed;
     private int messageSended;
     private int newViewers;
+    private int newFollowers;
     private HashMap<String, Integer> commandsUsed = new HashMap<>();
     private int bansAndTimeouts;
     private HashMap<String, Integer> usedEmotes = new HashMap<>();
+    private Message sessionMessage;
 
-    public Session(long currentTimeMillis) {
+    private String currentTitle;
+    private ArrayList<String> titles;
+    private String currentGameId;
+    private ArrayList<String> gameIds;
+
+    public Session(long currentTimeMillis, String channelId, BOT botDiscord) {
+        this.botDiscord = botDiscord;
+        this.channelId = channelId;
         this.startDateMillis = currentTimeMillis;
         this.uuid = UUID.randomUUID();
         this.maxViewers = 0;
@@ -25,10 +46,13 @@ public class Session {
         this.commandUsed = 0;
         this.messageSended = 0;
         this.newViewers = 0;
+        this.newFollowers = 0;
         this.bansAndTimeouts = 0;
     }
 
-    public Session(long currentTimeMillis, UUID uuid) {
+    public Session(long currentTimeMillis, UUID uuid, String channelId, BOT botDiscord) {
+        this.botDiscord = botDiscord;
+        this.channelId = channelId;
         this.startDateMillis = currentTimeMillis;
         this.uuid = uuid;
         this.maxViewers = 0;
@@ -36,11 +60,16 @@ public class Session {
         this.commandUsed = 0;
         this.messageSended = 0;
         this.newViewers = 0;
+        this.newFollowers = 0;
         this.bansAndTimeouts = 0;
     }
 
     public UUID getUuid() {
         return uuid;
+    }
+
+    public String getChannelId() {
+        return channelId;
     }
 
     public long getStartDate() {
@@ -123,6 +152,10 @@ public class Session {
         this.newViewers = newViewers;
     }
 
+    public void setChannelId(String channelId) {
+        this.channelId = channelId;
+    }
+
     public void setCommandsUsed(HashMap<String, Integer> commandsUsed) {
         this.commandsUsed = commandsUsed;
     }
@@ -133,6 +166,64 @@ public class Session {
 
     public void setUsedEmotes(HashMap<String, Integer> usedEmotes) {
         this.usedEmotes = usedEmotes;
+    }
+
+    public int getNewFollowers() {
+        return newFollowers;
+    }
+
+    public void setNewFollowers(int newFollowers) {
+        this.newFollowers = newFollowers;
+    }
+
+    public Message getSessionMessage() {
+        return sessionMessage;
+    }
+
+    public void setSessionMessage(Message sessionMessage) {
+        this.sessionMessage = sessionMessage;
+    }
+
+    public void newTitle(String newTitle){
+        this.titles.add(currentTitle);
+        this.currentTitle = newTitle;
+    }
+
+    public void newGame(String newGameId){
+        this.gameIds.add(newGameId);
+        this.currentGameId = newGameId;
+    }
+
+    public String getCurrentTitle() {
+        return currentTitle;
+    }
+
+    public void setCurrentTitle(String currentTitle) {
+        this.currentTitle = currentTitle;
+    }
+
+    public ArrayList<String> getTitles() {
+        return titles;
+    }
+
+    public void setTitles(ArrayList<String> titles) {
+        this.titles = titles;
+    }
+
+    public String getCurrentGameId() {
+        return currentGameId;
+    }
+
+    public void setCurrentGameId(String currentGameId) {
+        this.currentGameId = currentGameId;
+    }
+
+    public ArrayList<String> getGameIds() {
+        return gameIds;
+    }
+
+    public void setGameIds(ArrayList<String> gameIds) {
+        this.gameIds = gameIds;
     }
 
     public void addCommand() {
@@ -161,5 +252,37 @@ public class Session {
         int current = usedEmotes.get(emoteUsed);
         current++;
         usedEmotes.put(emoteUsed, current);
+    }
+
+    public void addFollower() {
+        newFollowers++;
+    }
+
+    public void updateMessage() {
+        Guild discord = botDiscord.getJda().getGuildById(Reference.GuildID.getString());
+        Member lyorine = discord.getMemberById(Reference.LyorineClientID.getString());
+        Role notif = discord.getRoleById(Reference.NotifRoleID.getString());
+        botDiscord.getLogger().log(Level.INFO, "> Le stream est ONLINE!");
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setTitle("Notification \uD83D\uDD14", "https://twitch.lyorine.com");
+        embedBuilder.setFooter(Reference.EmbedFooter.asDate(), Reference.EmbedIcon.getString());
+        embedBuilder.setColor(3066993);
+        embedBuilder.setDescription("Oyé oyé les "+notif.getAsMention()+" !\n**"+lyorine.getAsMention()+"** part en stream !\n» https://twitch.lyorine.com");
+        embedBuilder.addField(new MessageEmbed.Field("Titre", currentTitle, true));
+        GameList resultList = botDiscord.getTwitchClient().getHelix().getGames(Collections.singletonList(currentGameId), null).execute();
+        final String[] gameName = {"Aucun jeu"};
+        resultList.getGames().forEach(game -> {
+            gameName[0] = game.getName();
+            embedBuilder.setThumbnail(game.getBoxArtUrl());
+        });
+        embedBuilder.addField(new MessageEmbed.Field("Jeu", gameName[0], true));
+
+        StreamList streamResultList = botDiscord.getTwitchClient().getHelix().getStreams(botDiscord.getConfigurationManager().getStringValue("oauth2Token"), "", "", null, null, null, null, Collections.singletonList(channelId), null).execute();
+        final Stream[] currentStream = new Stream[1];
+        streamResultList.getStreams().forEach(stream -> {
+            currentStream[0] = stream;
+        });
+
+        embedBuilder.setImage(currentStream[0].getThumbnailUrl());
     }
 }

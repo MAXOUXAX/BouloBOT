dbe3cd3f3be982db3b1759600bfa2414240ad260
MAXOUXAX <djmax486@gmail.com>
2020-02-16 15:17:59 +0100

    Reverted dependencie due to lack of documentation

--- src/main/java/net/maxx/boulobot/BOT.java
+++ src/main/java/net/maxx/boulobot/BOT.java
@@ -121,6 +121,7 @@ public class BOT implements Runnable{
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
+                twitchClient.getEventManager().registerEventHandler();
                 twitchClient.getEventManager().onEvent(IRCMessageEvent.class).subscribe(event -> {
                     if(event.getMessage().isPresent()) {
                         String message = event.getMessage().get();

ac4001a842631d68e81d971cc72cd00af40f26fa
MAXOUXAX <djmax486@gmail.com>
2020-02-20 15:04:25 +0100

    Fixed a bug where sessions were empty

--- src/main/java/net/maxouxax/boulobot/commands/register/discord/CommandSession.java
+++ src/main/java/net/maxouxax/boulobot/commands/register/discord/CommandSession.java
@@ -21,16 +21,20 @@ public class CommandSession {
     public void session(User user, TextChannel textChannel, Message message, String[] args){
         textChannel.sendTyping().queue();
         StringBuilder strB = new StringBuilder();
-        botDiscord.getSessionManager().getSessions().forEach(session -> {
-            strB.append("\nsession.getUuid() = ").append(session.getUuid());
-            strB.append("\nsession.getAvgViewers() = ").append(session.getAvgViewers());
-            strB.append("\nsession.getBansAndTimeouts() = ").append(session.getBansAndTimeouts());
-            strB.append("\nsession.getChannelId() = ").append(session.getChannelId());
-            strB.append("\nsession.getCommandsUsed() = ").append(session.getCommandsUsed());
-            strB.append("\nsession.getGameIds().toString() = ").append(session.getGameIds().toString());
-            strB.append("\nsession.getStartDate() = ").append(session.getStartDate());
-        });
-        textChannel.sendMessage(strB.toString()).queue();
+        if(botDiscord.getSessionManager().getSessions().isEmpty()){
+            textChannel.sendMessage("Aucune session n'a été trouvée !").queue();
+        }else {
+            botDiscord.getSessionManager().getSessions().forEach(session -> {
+                strB.append("\nsession.getUuid() = ").append(session.getUuid());
+                strB.append("\nsession.getAvgViewers() = ").append(session.getAvgViewers());
+                strB.append("\nsession.getBansAndTimeouts() = ").append(session.getBansAndTimeouts());
+                strB.append("\nsession.getChannelId() = ").append(session.getChannelId());
+                strB.append("\nsession.getCommandsUsed() = ").append(session.getCommandsUsed());
+                strB.append("\nsession.getGameIds().toString() = ").append(session.getGameIds().toString());
+                strB.append("\nsession.getStartDate() = ").append(session.getStartDate());
+            });
+            textChannel.sendMessage(strB.toString()).queue();
+        }
     }
 
 }

3329f4ea7756080a3f703dffd7b91717f342e3c3
MAXOUXAX <djmax486@gmail.com>
2020-03-08 17:54:20 +0100

    Deleted Random command

--- src/main/java/net/maxouxax/boulobot/commands/CommandMap.java
+++ src/main/java/net/maxouxax/boulobot/commands/CommandMap.java
@@ -42,7 +42,7 @@ public final class CommandMap {
         this.botDiscord = botDiscord;
 
         registerCommands(new CommandDefault(botDiscord, this), new RoleCommand(botDiscord, this), new HelpCommand(this), new MusicCommand(botDiscord, this), new CommandWeather(botDiscord, this), new CommandNotif(botDiscord, this), new CommandChangelog(botDiscord, this), new CommandVersion(botDiscord, this), new CommandSession(botDiscord, this), new CommandOctogone(botDiscord, this), new CommandSay(botDiscord, this));
-        registerTwitchCommands(new TwitchWeather(botDiscord, this), new TwitchHelp(botDiscord, this), new TwitchKappa(botDiscord, this), new TwitchRandom(botDiscord, this), new TwitchNotif(botDiscord, this), new TwitchPwned(botDiscord, this), new TwitchVersion(botDiscord, this), new TwitchAquoijouer(botDiscord, this), new TwitchClipThat(botDiscord, this), new TwitchSCP(botDiscord, this));
+        registerTwitchCommands(new TwitchWeather(botDiscord, this), new TwitchHelp(botDiscord, this), new TwitchKappa(botDiscord, this), new TwitchNotif(botDiscord, this), new TwitchPwned(botDiscord, this), new TwitchVersion(botDiscord, this), new TwitchAquoijouer(botDiscord, this), new TwitchClipThat(botDiscord, this), new TwitchSCP(botDiscord, this));
 
         load();
     }
